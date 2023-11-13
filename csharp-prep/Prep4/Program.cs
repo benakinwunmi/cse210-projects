165a909896be9aadef32b3c24639709dbc4404b4
benakinwunmi <aki22006@byui.edu>
2023-11-13 12:05:43 +0100

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when you're done.");
+        int input;
+        do
+        {
+            Console.Write("Enter number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty(0).Min();
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        List<int> sortedList = numbers.OrderBy(n => n).ToList();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in sortedList)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
