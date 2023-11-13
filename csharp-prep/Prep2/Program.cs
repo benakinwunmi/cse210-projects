c4521429ccb5360cf9bde2ab76554b4c28b99d92
benakinwunmi <aki22006@byui.edu>
2023-11-13 11:37:35 +0100

    Prep2 grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,42 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+         double gradePercentage = Convert.ToDouble(Console.ReadLine());
+
+        char letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed.");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. Keep Trying!");
+        }
     }
 }
