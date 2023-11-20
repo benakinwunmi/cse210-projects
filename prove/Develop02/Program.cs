69702cb04b25476578d9510f91741343002a4c9c
benakinwunmi <aki22006@byui.edu>
2023-11-20 15:39:44 +0100

    edit and delete entries

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,140 +1,205 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-class JournalEntry
+// Entry class to represent a journal entry
+class Entry
 {
     public string Prompt { get; set; }
     public string Response { get; set; }
-    public string Date { get; set; }
+    public DateTime Date { get; set; }
+    public List<string> Tags { get; set; }
 
-    public JournalEntry(string prompt, string response, string date)
+    // Constructor for creating an entry
+    public Entry(string prompt, string response)
     {
         Prompt = prompt;
         Response = response;
-        Date = date;
+        Date = DateTime.Now;
+        Tags = new List<string>();
     }
 
-    public override string ToString()
+    // Display method to show the entry details
+    public void Display()
     {
-        return $"{Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+        Console.WriteLine($"Date: {Date.ToShortDateString()}");
+        Console.WriteLine($"Prompt: {Prompt}");
+        Console.WriteLine($"Response: {Response}");
+        Console.WriteLine($"Tags: {string.Join(", ", Tags)}\n");
     }
 }
 
+// Journal class to manage entries
 class Journal
 {
-    private List<JournalEntry> entries = new List<JournalEntry>();
+    private List<Entry> entries;
 
-    public void AddEntry(string prompt, string response, string date)
+    public Journal()
     {
-        JournalEntry entry = new JournalEntry(prompt, response, date);
-        entries.Add(entry);
+        entries = new List<Entry>();
     }
 
-    public void DisplayEntries()
+    // Method to add a new entry to the journal
+    public void AddEntry(string prompt, string response)
+    {
+        Entry newEntry = new Entry(prompt, response);
+        entries.Add(newEntry);
+    }
+
+    // Method to display all entries in the journal
+    public void DisplayJournal()
     {
         foreach (var entry in entries)
         {
-            Console.WriteLine(entry);
+            entry.Display();
         }
     }
 
+    // Method to save the journal to a file
     public void SaveToFile(string fileName)
     {
         using (StreamWriter outputFile = new StreamWriter(fileName))
         {
             foreach (var entry in entries)
             {
-                outputFile.WriteLine($"{entry.Date},{entry.Prompt},{entry.Response}");
+                // Save entry details to the file
+                outputFile.WriteLine($"{entry.Date},{entry.Prompt},{entry.Response},{string.Join(",", entry.Tags)}");
             }
         }
     }
 
+    // Method to load the journal from a file
     public void LoadFromFile(string fileName)
     {
-        entries.Clear(); // Clear existing entries before loading new ones
+        entries.Clear(); // Clear existing entries before loading from the file
 
-        string[] lines = File.ReadAllLines(fileName);
-
-        foreach (string line in lines)
+        try
         {
-            string[] parts = line.Split(',');
+            string[] lines = File.ReadAllLines(fileName);
 
-            string date = parts[0];
-            string prompt = parts[1];
-            string response = parts[2];
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split(",");
+                string prompt = parts[1];
+                string response = parts[2];
 
-            entries.Add(new JournalEntry(prompt, response, date));
+                // Parse the date from the file and create a new entry
+                if (DateTime.TryParse(parts[0], out DateTime date))
+                {
+                    Entry loadedEntry = new Entry(prompt, response)
+                    {
+                        Date = date
+                    };
+
+                    // Load tags if available
+                    if (parts.Length > 3)
+                    {
+                        loadedEntry.Tags.AddRange(parts[3].Split(','));
+                    }
+
+                    entries.Add(loadedEntry);
+                }
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found. Creating a new journal.");
+        }
+    }
+
+    // Method to edit an existing entry
+    public void EditEntry(int entryIndex, string newResponse)
+    {
+        if (entryIndex >= 0 && entryIndex < entries.Count)
+        {
+            entries[entryIndex].Response = newResponse;
+        }
+    }
+
+    // Method to delete an existing entry
+    public void DeleteEntry(int entryIndex)
+    {
+        if (entryIndex >= 0 && entryIndex < entries.Count)
+        {
+            entries.RemoveAt(entryIndex);
         }
     }
 }
 
+// Program class containing the main entry point
 class Program
 {
     static void Main()
     {
-        Journal journal = new Journal();
+        Journal myJournal = new Journal();
+
+        Console.WriteLine("Welcome to the Journal App!");
 
         while (true)
         {
-            Console.WriteLine("\n1. Begin a new journal entry");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
-            Console.WriteLine("5. Exit");
+            // Display menu options including entry editing and deletion
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Edit an existing entry");
+            Console.WriteLine("4. Delete an existing entry");
+            Console.WriteLine("5. Save the journal to a file");
+            Console.WriteLine("6. Load the journal from a file");
+            Console.WriteLine("7. Exit");
 
-            Console.Write("Enter your choice (1-5): ");
+            Console.Write("Enter your choice (1-7): ");
             string choice = Console.ReadLine();
 
             switch (choice)
             {
                 case "1":
-                    string randomPrompt = GetRandomPrompt();
-                    Console.WriteLine($"Prompt: {randomPrompt}");
-                    Console.Write("Your response: ");
+                    Console.Write("Enter your response to the prompt: ");
                     string response = Console.ReadLine();
-                    string currentDate = DateTime.Now.ToShortDateString();
-                    journal.AddEntry(randomPrompt, response, currentDate);
+                    myJournal.AddEntry("Sample Prompt", response);
                     break;
+
                 case "2":
-                    journal.DisplayEntries();
+                    myJournal.DisplayJournal();
                     break;
+
                 case "3":
-                    Console.Write("Enter a filename to save the journal: ");
-                    string saveFileName = Console.ReadLine();
-                    journal.SaveToFile(saveFileName);
-                    Console.WriteLine("Journal saved successfully!");
+                    Console.Write("Enter the index of the entry to edit: ");
+                    if (int.TryParse(Console.ReadLine(), out int editIndex))
+                    {
+                        Console.Write("Enter the new response: ");
+                        string newResponse = Console.ReadLine();
+                        myJournal.EditEntry(editIndex, newResponse);
+                    }
                     break;
+
                 case "4":
-                    Console.Write("Enter a filename to load the journal: ");
-                    string loadFileName = Console.ReadLine();
-                    journal.LoadFromFile(loadFileName);
-                    Console.WriteLine("Journal loaded successfully!");
+                    Console.Write("Enter the index of the entry to delete: ");
+                    if (int.TryParse(Console.ReadLine(), out int deleteIndex))
+                    {
+                        myJournal.DeleteEntry(deleteIndex);
+                    }
                     break;
+
                 case "5":
+                    Console.Write("Enter the file name to save: ");
+                    string saveFileName = Console.ReadLine();
+                    myJournal.SaveToFile(saveFileName);
+                    break;
+
+                case "6":
+                    Console.Write("Enter the file name to load: ");
+                    string loadFileName = Console.ReadLine();
+                    myJournal.LoadFromFile(loadFileName);
+                    break;
+
+                case "7":
                     Environment.Exit(0);
                     break;
+
                 default:
-                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 7.");
                     break;
             }
         }
-    }
-
-    static string GetRandomPrompt()
-    {
-        // List of prompts
-        List<string> prompts = new List<string>
-        {
-            "Who was the most interesting person I interacted with today?",
-            "What was the best part of my day?",
-            "How did I see the hand of the Lord in my life today?",
-            "What was the strongest emotion I felt today?",
-            "If I had one thing I could do over today, what would it be?"
-        };
-
-        Random random = new Random();
-        int index = random.Next(prompts.Count);
-        return prompts[index];
     }
 }
