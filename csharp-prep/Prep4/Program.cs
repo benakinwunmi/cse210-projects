c38e289788b72f2595d405bdd8a8518e86029269
benakinwunmi <aki22006@byui.edu>
2023-11-21 11:24:40 +0100

    week 4 learning components

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,42 +1,13 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
 
-        List<int> numbers = new List<int>();
-
-        Console.WriteLine("Enter a list of numbers, type 0 when you're done.");
-        int input;
-        do
-        {
-            Console.Write("Enter number: ");
-            input = Convert.ToInt32(Console.ReadLine());
-            if (input != 0)
-            {
-                numbers.Add(input);
-            }
-        } while (input != 0);
-
-        int sum = numbers.Sum();
-        Console.WriteLine($"The sum is: {sum}");
-
-        float average = ((float)sum) / numbers.Count();
-        Console.WriteLine($"The average is: {average}");
-
-        int max = numbers.Max();
-        Console.WriteLine($"The largest number is: {max}");
-
-        int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty(0).Min();
-        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
-
-        List<int> sortedList = numbers.OrderBy(n => n).ToList();
-        Console.WriteLine("The sorted list is:");
-        foreach (int num in sortedList)
-        {
-            Console.WriteLine(num);
-        }
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
