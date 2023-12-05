d6754833007b1b482474a187d94645fb4f2d1b66
benakinwunmi <aki22006@byui.edu>
2023-12-05 13:22:55 +0100

    w5 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,119 @@
 using System;
 
+using System;
+using System.Collections.Generic;
+
+class Shape
+{
+    private string color;
+
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0.0;
+    }
+}
+
+class Square : Shape
+{
+    private double side;
+
+    public double Side
+    {
+        get { return side; }
+        set { side = value; }
+    }
+
+    public Square(string color, double side) : base(color)
+    {
+        Side = side;
+    }
+
+    public override double GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public double Length
+    {
+        get { return length; }
+        set { length = value; }
+    }
+
+    public double Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+
+    public Rectangle(string color, double length, double width) : base(color)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public override double GetArea()
+    {
+        return Length * Width;
+    }
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+
+    public Circle(string color, double radius) : base(color)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 5.0);
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Circle circle = new Circle("Green", 3.0);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.Color}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
